1d3736f7ef677cb4f9efd165977a5275e37fabf2
Carl Noval <cjrnoval22@gmail.com>
2022-07-04 21:36:05 +0900

    dynamic gif id

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -3,8 +3,9 @@ import React, { Component } from 'react'; // every new jsx file would have this 
 
 class Gif extends Component {
   render() {
+    const { id } = this.props;
     // url came from giphy's website after inspecting some gif
-    const src = "https://media2.giphy.com/media/0qGegxoMTCalyRaN0h/200w.gif";
+    const src = `https://media2.giphy.com/media/${id}/200.gif`;
     // const lwGif = "https://media2.giphy.com/media/xT9IgDEI1iZyb2wqo8/200.gif";
     return (
       <img src={src} alt="" className="gif" />
