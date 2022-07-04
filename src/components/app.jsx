3df9a6479956a97e3b7aed64c8a7e41535682ab9
Carl Noval <cjrnoval22@gmail.com>
2022-07-04 21:06:36 +0900

    app component updated from function to class and also started using search bar component

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,28 +1,31 @@
-// import React, { Component } from 'react'; // every new jsx file would have this line
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react'; // every new jsx file would have this line
+import SearchBar from './search_bar';
 
-// // component declared as a class
-// class App extends Component {
-//   render () {
-//     return (
-//       <div>
-//         <div className="left-scene" />
-//         <div className="right-scene" />
-//       </div>
-//     );
-//   }
-// }
+// component declared as a class
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <div className="left-scene">
+          <SearchBar />
+        </div>
+        <div className="right-scene" />
+      </div>
+    );
+  }
+}
 
+// // if declared as a function instead of a class
+// import React from 'react'; // every new jsx file would have this line
 
-import React from 'react'; // every new jsx file would have this line
-
-// component declared as a function cause there are no events yet
-const App = () => {
-  return (
-    <div>
-      <div className="left-scene" />
-      <div className="right-scene" />
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <div className="left-scene" />
+//       <div className="right-scene" />
+//     </div>
+//   );
+// };
 
 export default App;
