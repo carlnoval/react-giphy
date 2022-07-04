dbd5f34c32483002db45d4b7d50c028cfea598df
Carl Noval <cjrnoval22@gmail.com>
2022-07-04 20:44:49 +0900

    the app comonent

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,28 @@
-import React, { Component } from 'react'; // every new jsx file would have this line
-import ReactDOM from 'react-dom';
-import '../../assets/stylesheets/application.scss';
+// import React, { Component } from 'react'; // every new jsx file would have this line
 
-class App extends Component {
-  render () {
-    return <div>
-      <div className="left-scene"></div>
-      <div className="right-scene"></div>
+// // component declared as a class
+// class App extends Component {
+//   render () {
+//     return (
+//       <div>
+//         <div className="left-scene" />
+//         <div className="right-scene" />
+//       </div>
+//     );
+//   }
+// }
+
+
+import React from 'react'; // every new jsx file would have this line
+
+// component declared as a function cause there are no events yet
+const App = () => {
+  return (
+    <div>
+      <div className="left-scene" />
+      <div className="right-scene" />
     </div>
-  }
-}
+  );
+};
 
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.render(<App />, root);
-}
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,12 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { Component } from 'react'; // every new jsx file would have this line
+import React from 'react'; // every new jsx file would have this line
 import ReactDOM from 'react-dom';
 import '../assets/stylesheets/application.scss';
-
-
+import App from './components/app';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<Hello name="Sekai" />, root);
+  ReactDOM.render(<App />, root);
 }
