b009fc8bda27caee67f6c1ea30868ce13b7d5292
Carl Noval <cjrnoval22@gmail.com>
2022-07-05 14:46:52 +0900

    minor refactor, 1 line per attribute

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -2,10 +2,17 @@
 import React, { Component } from 'react'; // every new jsx file would have this line
 
 class SearchBar extends Component {
+  handleGifListUpdate = (event) => {
+    console.log(event.target.value);
+  }
+
   render() {
     return (
-      // form-control is from bootstrap
-      <input type="text" className="form-control form-search" />
+      <input
+        type="text"
+        className="form-control form-search" // form-control is from bootstrap
+        onChange={this.handleGifListUpdate}
+      />
     );
   }
 }
