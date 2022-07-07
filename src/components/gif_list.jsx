97424782126922e669060d85c5e8e74c0ef13a9b
Carl Noval <cjrnoval22@gmail.com>
2022-07-08 01:13:08 +0900

    fleshed out comments on gif_list.jsx

--- src/components/gif_list.jsx
+++ src/components/gif_list.jsx
@@ -1,12 +1,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react'; // every new jsx file would have this line
 
+// this allows use of the html return by gif.jsx module
 import Gif from './gif';
 
 class GifList extends Component {
   renderList = () => {
+    // gifs is either an object/array from `App` module
+    // object contains object - array contains object
     const { gifs } = this.props;
+
+    // console.log(gifs); // log and/or put a breakpoint here for debugging
+
+    // renderList retuns a list of html returned by the component from gif.jsx
     return gifs.map((gif) => {
+      // console.log(gifs); // log and/or put a breakpoint here for debugging
+      // gif here would always be an object
+      // Gif component returns an img tag with a url using gif.id in the src attribute
       return <Gif id={gif.id} key={gif.id} />; // key is to get rid of the react warning
     });
   }
@@ -14,6 +24,7 @@ class GifList extends Component {
   render() {
     // javascript injection as attribute value should be inside brackets{}
     return (
+      // gif-list div will contain all the html returned by Gif component
       <div className="gif-list">
         {this.renderList()}
       </div>
@@ -21,4 +32,5 @@ class GifList extends Component {
   }
 }
 
+// so that app.jsx module can use the component in this module
 export default GifList;
